a1abe15ccafc5022d8079a3bcb8fbd69e4ca2a47
martinolee <martino_lee@naver.com>
2020-05-26 22:18:14 +0900

    update AuthService

---
+++ Messenger/Messenger/Source/Services/AuthService.swift
@@ -0,0 +1,74 @@
+//
+//  AuthService.swift
+//  Messenger
+//
+//  Created by Soohan Lee on 2020/05/18.
+//  Copyright © 2020 Soohan Lee. All rights reserved.
+//
+
+import Foundation
+import FirebaseAuth
+
+final public class AuthService {
+  public static let shared = AuthService()
+  
+  private let auth = Auth.auth()
+  
+  private let userDB = DatabaseService.shared.usersDB
+  
+  public var isUserLoggedIn: Bool {
+    auth.currentUser != nil
+  }
+  
+  public func signUp(name: String, email: String, password: String) -> Observable<AuthDataResult> {
+    return Observable.create { observer in
+      self.auth.createUser(withEmail: email, password: password) { result, error in
+        if let error = error {
+          observer.onError(error)
+        } else if let result = result {
+          observer.onNext(result)
+          
+          DatabaseService.shared.addNewUser(uid: result.user.uid, name: name, email: email) { error in
+            if let error = error {
+              observer.onError(error)
+            }
+          }
+        }
+        
+        observer.onCompleted()
+        return
+      }
+      return Disposables.create()
+    }
+  }
+  
+  public func logIn(email: String, password: String) -> Observable<AuthDataResult> {
+    return Observable.create { observer in
+      self.auth.signIn(withEmail: email, password: password) { result, error in
+        if let error = error {
+          observer.onError(error)
+        } else if let result = result {
+          observer.onNext(result)
+        }
+        
+        observer.onCompleted()
+        return
+      }
+      return Disposables.create()
+    }
+  }
+  
+  public func deleteAccount(completionHandler: @escaping (Error?) -> Void) {
+    guard let uid = auth.currentUser?.uid else { return }
+    
+    userDB.document(uid).delete { error in
+      if let error = error { completionHandler(error) }
+
+      self.auth.currentUser?.delete(completion: { error in
+        completionHandler(error)
+      })
+    }
+  }
+  
+  private init() { }
+}
