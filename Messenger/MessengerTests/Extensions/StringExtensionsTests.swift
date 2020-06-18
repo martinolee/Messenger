10a9eb5955411c787ac52adf28e8d03cd06c6b0e
martinolee <soohan.m.lee@gmail.com>
2020-06-18 20:46:56 +0900

    add test case

--- Messenger/MessengerTests/Extensions/StringExtensionsTests.swift
+++ Messenger/MessengerTests/Extensions/StringExtensionsTests.swift
@@ -1,5 +1,5 @@
 //
-//  StringAdditionsTests.swift
+//  StringExtensionsTests.swift
 //  MessengerTests
 //
 //  Created by Soohan Lee on 2020/06/17.
@@ -10,7 +10,8 @@
 
 import XCTest
 
-class StringAdditionsTests: XCTestCase {
+class StringExtensionsTests: XCTestCase {
+  
   func testIsValidEmail() {
     let validEmail = "soohan.m.lee@gmail.com"
     let invalidEmail = "This is invalid email"
@@ -18,5 +19,14 @@ class StringAdditionsTests: XCTestCase {
     XCTAssert(validEmail.isValidEmail == true, "It has invalid email")
     
     XCTAssert(invalidEmail.isValidEmail == false, "It has valid email")
+  }
+  
+  func testHasWhiteSpace() {
+    let stringWithWhiteSpace = " String with white space "
+    let stringWithoutWhiteSpace = "String without white space"
+    
+    XCTAssert(stringWithWhiteSpace.hasWhiteSpace == true, "It has not white space")
+    
+    XCTAssert(stringWithoutWhiteSpace.hasWhiteSpace == false, "It has white space")
   }
 }
