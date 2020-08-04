80561fdf9a6c52c7ac3e560e15a293b10744c097
martinolee <soohan.m.lee@gmail.com>
2020-08-04 20:38:19 +0900

    Add Password

---
+++ Messenger/Messenger/Source/Models/Password.swift
@@ -0,0 +1,11 @@
+//
+//  Password.swift
+//  Messenger
+//
+//  Created by Soohan Lee on 2020/08/04.
+//  Copyright © 2020 Soohan Lee. All rights reserved.
+//
+
+import Foundation
+
+typealias Password = String
