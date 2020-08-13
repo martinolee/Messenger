6a157ce87532681414f402a68b4ba2319abd7c06
martinolee <soohan.m.lee@gmail.com>
2020-08-13 22:50:47 +0900

    Add SignupService

---
+++ Messenger/Messenger/Source/Services/SignupService.swift
@@ -0,0 +1,12 @@
+//
+//  SignupService.swift
+//  Messenger
+//
+//  Created by Soohan Lee on 2020/08/13.
+//  Copyright © 2020 Soohan Lee. All rights reserved.
+//
+
+import Foundation
+
+protocol SignupService {
+}
