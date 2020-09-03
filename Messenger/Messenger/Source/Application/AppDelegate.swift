19b70c0e36a4c544b7484bb9f2226a85a669a810
martinolee <soohan.m.lee@gmail.com>
2020-09-03 23:02:42 +0900

    Update convention for AppDelegate

--- Messenger/Messenger/Source/Application/AppDelegate.swift
+++ Messenger/Messenger/Source/Application/AppDelegate.swift
@@ -27,7 +27,10 @@ final class AppDelegate: UIResponder, UIApplicationDelegate {
     configurationForConnecting connectingSceneSession: UISceneSession,
     options: UIScene.ConnectionOptions
   ) -> UISceneConfiguration {
-    return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    return UISceneConfiguration(
+      name: "Default Configuration",
+      sessionRole: connectingSceneSession.role
+    )
   }
   
 }
