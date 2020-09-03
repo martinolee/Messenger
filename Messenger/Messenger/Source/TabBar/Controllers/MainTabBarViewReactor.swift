d55e6183ca4621b10c0b6aae93754449b85b55ed
martinolee <soohan.m.lee@gmail.com>
2020-09-03 23:04:13 +0900

    Add MainTabBarViewReactor

---
+++ Messenger/Messenger/Source/TabBar/Controllers/MainTabBarViewReactor.swift
@@ -0,0 +1,18 @@
+//
+//  MainTabBarViewReactor.swift
+//  Messenger
+//
+//  Created by Soohan Lee on 2020/09/02.
+//  Copyright © 2020 Soohan Lee. All rights reserved.
+//
+
+import ReactorKit
+
+final class MainTabBarViewReactor: Reactor {
+  typealias Action = NoAction
+  
+  struct State {
+  }
+  
+  let initialState = State()
+}
